f7b2f6bfd184fd7c6da614dc3a98c01c5770e1ee
Sherlock <cerahoeug@gmail.com>
2026-05-08 13:13:17 +0200

    Create test5.py

---
+++ test5.py
@@ -0,0 +1,27 @@
+def addition(a, b):
+    return a + b
+def soustraction(a, b):
+    return a - b
+def multiplication(a, b):
+    return a * b
+def division(a, b):
+    if b == 0:
+        return "Erreur : division par zéro"
+    return a / b
+print("1 : Addition")
+print("2 : Soustraction")
+print("3 : Multiplication")
+print("4 : Division")
+choix = input("Choisis une opération (1/2/3/4) : ")
+a = float(input("Entre le premier nombre : "))
+b = float(input("Entre le deuxième nombre : "))
+if choix == "1":
+    print("Résultat :", addition(a, b))
+elif choix == "2":
+    print("Résultat :", soustraction(a, b))
+elif choix == "3":
+    print("Résultat :", multiplication(a, b))
+elif choix == "4":
+    print("Résultat :", division(a, b))
+else:
+    print("Choix invalide")
